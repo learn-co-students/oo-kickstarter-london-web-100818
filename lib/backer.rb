56ae592de2711eeef2ee76d107ef3622a2bf1384
ju-zp <jhuntington@tutanota.com>
2018-10-12 08:59:41 +0100

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Backer
+
+    attr_reader :name, :backed_projects
+
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+    end
+
+    def back_project(project)
+        self.backed_projects << project
+        if !project.backers.include? self
+            project.add_backer(self)
+            #puts "yes"
+        end
+    end
+
+end
